452f781ac5450b827f8ee42db7e1bfdba566df8b
MUHAMMAD USAMA AKBAR <106468370+usamabinakbar@users.noreply.github.com>
2025-10-18 23:05:53 +0500

    Replace ArrayAnalyzer with PackageDimensions class
    Removed ArrayAnalyzer class and added PackageDimensions class to calculate package dimensions and volume.

--- ArrayAnalyzer.java
+++ ArrayAnalyzer.java
@@ -1,22 +1,22 @@
-public class ArrayAnalyzer {
-    public static int findLargestIndex(int[] data) {
-        if (data == null || data.length == 0) {
-            return -1;
-        }
-        int maxIndex = 0;
-        int maxValue = data[0];
-        for (int i = 1; i <= data.length; i++) {
-            if (data[i] > maxValue) {
-                maxValue = data[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
+import java.util.Scanner;
 
+public class PackageDimensions {
     public static void main(String[] args) {
-        int[] numbers = {10, 5, 42, 88, 15, 30};
-        int resultIndex = findLargestIndex(numbers);
-        System.out.println("The index of the largest element is: " + resultIndex);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter item length: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter item width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter item height: ");
+        double height = scanner.nextDouble();
+
+        // Add 2 units padding to each dimension for package
+        double packageLength = length + 4; // 2 on each side
+        double packageWidth = width + 4;
+        double packageHeight = height + 4;
+        double volume = packageLength * packageWidth * packageHeight;
+
+        System.out.println("Package dimensions: " + packageLength + " x " + packageWidth + " x " + packageHeight);
+        System.out.println("Package volume: " + volume);
     }
 }
